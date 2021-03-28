f55632315efbe3bc1b718ebd74e67029fb82d4a7
Bunta Ervin-Robert <60225362+ervin-designs-eu@users.noreply.github.com>
2021-03-29 01:48:17 +0300

    Add files via upload

---
+++ webbyilimo/webbyilimo/konfig.php
@@ -0,0 +1,15 @@
+﻿<?php
+/* Adatbázis kezelés, Ranglista a dashboardra */
+define('DB_SERVER', '');
+define('DB_USERNAME', '');
+define('DB_PASSWORD', '');
+define('DB_NAME', '');
+ 
+// FOR ANY OTHER IMPLEMENTS CONTACT ME!
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Ellenorzes
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
